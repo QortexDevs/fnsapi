9546f122ad4a1ffc325ec565aab96127fcf3636f
Nick Mitin <it@qortex.ru>
2020-12-29 08:35:19 +0300

    Updated README.md

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='fns-api',
+    name='fnsapi',
     version='0.0.1',
     author='Qortex Devs',
     author_email='it@qortex.ru',
     description='Russian Federal Tax Service API connector',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/QortexDevs/fns-api',
+    url='https://github.com/QortexDevs/fnsapi',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
